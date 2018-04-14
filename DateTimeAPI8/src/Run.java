b67d1c1246e5d1e82fd325cd3f3d495883d40875
Bushy.Netshidaulu <Bushy.Netshidaulu@03rnbc-ntpc0204.za.ds.naspers.com>
2018-04-14 22:09:27 +0200

    current Date

---
+++ DateTimeAPI8/src/Run.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Run {
+    public static void main(String[] args){
+        LocalDate date = LocalDate.now();//get the current Date
+        System.out.println("Current Date :"+date);
+
+
+    }
+}
